7a0814efd0474f04c9bfc1c5234f6a84de7a27c8
wai81 <50743966+wai81@users.noreply.github.com>
2021-11-15 23:32:09 +0300

    Initial commit

---
+++ task1_airline/AirlineCompany.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task1_airline.Aircrafts;
+
+namespace task1_airline
+{
+    class AirlineCompany
+    {
+        public Guid Id { get; set; }
+        public string NameCompany { get; set; }
+        public List<Airplane> Aircrafts { get; set; }
+
+        public AirlineCompany()
+        {
+
+        }
+    }
+}
---
+++ task1_airline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task1_airline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
